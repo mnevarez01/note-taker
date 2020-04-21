944454a592d07f1b8bcb878cd2e77a98e5baeb78
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-21 17:50:02 -0600

    added app.get and listen to server.js

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const path = require("path")
+const fs = require("fs")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log(`App listening on PORT: ${PORT}`)
+})
