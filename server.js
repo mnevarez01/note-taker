c3bd01639da49d03ae495d0ec4f272b949e9f6a3
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-21 18:20:37 -0600

    added routes file and updated server.js

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const path = require("path")
 const fs = require("fs")
+const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
-});
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-});
-
-
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 
 app.listen(PORT, function () {
     console.log(`App listening on PORT: ${PORT}`)
 })
+
+
+
+
+// app.get("/notes", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/public/notes.html"))
+// });
+
+// app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/public/index.html"))
+// });
+
+
+// app.get("/api/notes", function (req, res){
+//     fs.readFile("db.json", "utf8", function(){
+
+//     })
+// })
