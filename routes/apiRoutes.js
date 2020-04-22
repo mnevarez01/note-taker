318b6e0405d79e01b4cbab491d02d076919e7db1
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-21 19:07:12 -0600

    updated store.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
 
-const store = 
+const store = require("../db/store");
+
+router.get("/notes", function (req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+})
